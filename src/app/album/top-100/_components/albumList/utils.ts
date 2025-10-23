97ab5395b58813c87f6fdd0c2286c32df684eb8d
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2025-10-23 17:22:36 +0200

    chore(UI): removed Input

---
+++ src/app/album/top-100/_components/albumList/utils.ts
@@ -0,0 +1,56 @@
+import { AlbumEntry } from "@THTS/types/album/albumEntry";
+
+type FilterAlbumProps = {
+  query: {
+    search: string;
+    genre: string[];
+    artist: string;
+    year: string;
+  };
+  albums: AlbumEntry[];
+};
+
+export const filterAlbum = ({
+  query,
+  albums,
+}: FilterAlbumProps): AlbumEntry[] => {
+  if (!albums || albums.length === 0) {
+    return [];
+  }
+
+  return albums.filter((album) => {
+    if (query.search) {
+      const searchLower = query.search.toLowerCase();
+      const albumNameMatch = album["im:name"].label
+        .toLowerCase()
+        .includes(searchLower);
+
+      if (!albumNameMatch) return false;
+    }
+
+    if (query.artist) {
+      const artistLower = query.artist.toLowerCase();
+      const artistMatch = album["im:artist"].label
+        .toLowerCase()
+        .includes(artistLower);
+
+      if (!artistMatch) return false;
+    }
+
+    if (query.genre && query.genre.length > 0) {
+      const genreMatch = query.genre.includes(album.category.attributes.label);
+
+      if (!genreMatch) return false;
+    }
+
+    if (query.year) {
+      const albumYear = new Date(album["im:releaseDate"].label)
+        .getFullYear()
+        .toString();
+
+      if (albumYear !== query.year) return false;
+    }
+
+    return true;
+  });
+};
