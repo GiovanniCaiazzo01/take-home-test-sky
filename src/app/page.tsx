f825871dacd5fd076e20112532b4bb70bef77ab7
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2025-10-25 00:14:24 +0200

    chore: redirect to the page of interest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Text } from "@THTS/UI/components/Text";
+import { redirect } from "next/navigation"
 
 export default function Home() {
-  return <Text variant="title-lg">Test Title</Text>;
+  redirect("/album/top-100")
 }
